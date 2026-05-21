2823fb3fb08151792b2008c0250171ef2d614fd0
PythagorasGit <thomas.gomezr@upb.edu.co>
2026-05-21 12:10:00 -0500

    feat(HU-86): implement fighter registration

--- src/api/PeleadoresApi.ts
+++ src/api/PeleadoresApi.ts
@@ -10,6 +10,34 @@ export const getPeleadores = async (): Promise<Peleador[]> => {
   return response.data;
 };
 
+export const createPeleador = async (peleador: PeleadorPost): Promise<void> => {
+  const formData = new FormData();
+
+  formData.append("nombre", peleador.nombre);
+  formData.append("apodo", peleador.apodo);
+  formData.append("genero", peleador.genero);
+
+  formData.append("img", peleador.img);
+
+  formData.append("esNuevo", String(peleador.esNuevo));
+
+  formData.append("peso", peleador.peso);
+  formData.append("altura", peleador.altura);
+  formData.append("alcance", peleador.alcance);
+  formData.append("edad", peleador.edad);
+
+  formData.append("ultimaPelea", peleador.ultimaPelea);
+  formData.append("duracionPromedioEnPelea", peleador.duracionPromedioEnPelea);
+
+  formData.append("victorias", peleador.victorias);
+  formData.append("derrotas", peleador.derrotas);
+  formData.append("empates", peleador.empates);
+  formData.append("rachaActual", peleador.rachaActual);
+  formData.append("rachaHistorica", peleador.rachaHistorica);
+
+  await api.post(FIGHTERS_BASE, formData);
+};
+
 export const toggleFighterStatus = async (id: string): Promise<void> => {
   // PATCH /api/fighters/{id}/status
   await api.patch(`${FIGHTERS_BASE}/${id}/status`);
