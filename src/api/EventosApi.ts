1b0f76179f8d1edb54c15d6e15312a5427d60eaf
PythagorasGit <thomas.gomezr@upb.edu.co>
2026-05-21 12:16:17 -0500

    feat(HU-89): implement event creation

--- src/api/EventosApi.ts
+++ src/api/EventosApi.ts
@@ -5,7 +5,7 @@ import {ResultadoPost} from "@/src/types/POST/ResultadoPost.ts";
 import {Resultado} from "@/src/types/GET/Resultado.ts";
 import {EditEventPost} from "@/src/types/POST/EditEventPost.ts";
 
-const FIGHT_BASE = "/fight";
+const FIGHT_BASE = "/fight"; 
 
 export const getEventos = async (): Promise<Evento[]> => {
   const response = await api.get<Evento[]>(FIGHT_BASE);
@@ -15,4 +15,24 @@ export const getEventos = async (): Promise<Evento[]> => {
 export const getEventosLibres = async (): Promise<Evento[]> => {
   const response = await api.get<Evento[]>(`${FIGHT_BASE}/free`);
   return response.data;
+};
+
+export const createEvent = async (evento: EventoPost): Promise<void> => {
+  // POST /api/fight
+  await api.post(FIGHT_BASE, evento);
+};
+
+export const finishEvent = async (resultado: ResultadoPost): Promise<void> => {
+  // POST /api/fight/result
+  await api.post(`${FIGHT_BASE}/result`, resultado);
+};
+
+export const editEvent = async (editEvento:EditEventPost): Promise<void> => {
+  // PUT /api/fight/edit
+  await api.put(`${FIGHT_BASE}/edit`, editEvento);
+};
+
+export const cancelEvent = async (fightId: string): Promise<void> => {
+  // POST /api/fight/{fightId}/status
+  await api.patch(`${FIGHT_BASE}/${fightId}/status`);
 };
