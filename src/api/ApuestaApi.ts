78fe022251dfe1189aa220395543776bcc77c761
PythagorasGit <thomas.gomezr@upb.edu.co>
2026-05-29 10:36:02 -0500

    feat(HU-95): implement bet visualization by event

--- src/api/ApuestaApi.ts
+++ src/api/ApuestaApi.ts
@@ -1,8 +1,19 @@
 import api from "./AxiosClient.ts";
-
+import {Apuesta} from "@/src/types/GET/Apuesta.ts";
+import {UserApuesta} from "@/src/types/GET/UserApuesta.ts";
 import {UserApuestaPost} from "@/src/types/POST/UserApuestaPost.ts";
 
 const BET_BASE = "/bet";
+
+export const getApuestas = async (): Promise<Apuesta[]> => {
+  const response = await api.get<Apuesta[]>(BET_BASE);
+  return response.data;
+};
+
+export const getUserApuestas = async (): Promise<UserApuesta[]> => {
+  const response = await api.get<UserApuesta[]>(`${BET_BASE}/user`);
+  return response.data;
+};
 
 export const createUserApuesta = async (userApuestaPet: UserApuestaPost): Promise<void> => {
   await api.post(`${BET_BASE}/stake`, userApuestaPet);
